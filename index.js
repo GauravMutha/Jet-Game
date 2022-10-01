811ba8364ad975b1eafdf72541a7d8021853e4dd
Gaurav Mutha <gauravmutha1412@gmail.com>
2022-10-02 00:09:08 +0530

    Add Plane's movement

--- index.js
+++ index.js
@@ -34,13 +34,47 @@ function animate(){
     ctx.drawImage(image, 0,y1);
     ctx.drawImage(image, 0,y2);
     ctx.drawImage(planeImage,(sprite.frameX)*spriteWidth,(sprite.frameY)*spriteHeight,spriteWidth,spriteHeight,plane.x,plane.y,spriteWidth,spriteHeight);
-    
+    newpos();
     y1+=gamespeed;
     y2+=gamespeed;
     if(y1>722) y1=-722+y2;
     if(y2>722) y2=-722+y1;
-
+`   `
     requestAnimationFrame(animate);
 }
+
+function kd(e){
+    if(e.key=='ArrowRight'){
+        plane.dx+=plane.speed;
+        sprite.frameX=3;
+    }
+    if(e.key=='ArrowLeft'){
+        plane.dx-=plane.speed
+        sprite.frameX=1;
+    }
+}
+function ku(e){
+    if(e.key=='ArrowLeft' || e.key=='ArrowRight'){
+        plane.dx=0;
+        sprite.frameX=2;
+    }
+}
+
+function detectwalls(){
+    if(plane.x<-2)
+        plane.x=-2;
+    if(plane.x+spriteWidth>CANVAS_WIDTH)
+        plane.x=CANVAS_WIDTH-spriteWidth
+}
+
+function newpos(){
+    plane.x+=plane.dx;
+    plane.y+=plane.dy;
+
+    detectwalls();
+}
+
+document.addEventListener("keydown" ,kd);
+document.addEventListener("keyup", ku);
 
 animate();
